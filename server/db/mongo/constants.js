589e31b8ae7afb29418bc0ff5b5be0da1f90911e
Alexis Deschamps <aldeschamps@chatham.teksavvy.ca>
2017-02-14 22:47:51 -0500

    Heroku change

--- server/db/mongo/constants.js
+++ server/db/mongo/constants.js
@@ -1,4 +1,4 @@
-export const db = 'mongodb://TestTempPBUser:TestTempPBPass@ds147905.mlab.com:47905/portfolio-balancer';
+export const db = 'mongodb://' + process.env.PORTFOLIO_REBALANCER_USERNAME + ':' + process.env.PORTFOLIO_REBALANCER_PASSWORD + process.env.MONGODB_URI;
 
 export default {
   db
