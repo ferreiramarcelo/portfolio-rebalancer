d0cc3d9f78ad3f0d2c2bd6dbeff3e3d968ad0b5a
Alexis Deschamps <aldeschamps@chatham.teksavvy.ca>
2017-02-15 12:52:07 -0500

    Setup env variables for dev env unsing dotenv

--- server/db/mongo/constants.js
+++ server/db/mongo/constants.js
@@ -1,4 +1,18 @@
-export const db = 'mongodb://' + process.env.PORTFOLIO_REBALANCER_USERNAME + ':' + process.env.PORTFOLIO_REBALANCER_PASSWORD + process.env.MONGODB_URI;
+import { ENV } from '../../config/appConfig';
+import dotenv from 'dotenv';
+
+var dbURI = '';
+if (ENV === 'production') {
+  dbURI = 'mongodb://' + process.env.PORTFOLIO_REBALANCER_USERNAME + ':' + process.env.PORTFOLIO_REBALANCER_PASSWORD + process.env.MONGODB_URI;
+}
+else {
+  dotenv.load();
+  //import { ENV } from '../../../config/appConfig';
+  console.log("USERNAME" + process.env.PORTFOLIO_REBALANCER_USERNAME_DEV );
+  dbURI = 'mongodb://' + process.env.PORTFOLIO_REBALANCER_USERNAME_DEV + ':' + process.env.PORTFOLIO_REBALANCER_PASSWORD_DEV + process.env.MONGODB_URI_DEV;
+  //dbURI = 'mongodb://alsoeasierrailroadstone:eachlittlealsostop@ds153669.mlab.com:53669/portfolio-rebalancer';
+}
+export const db = dbURI;
 
 export default {
   db
