fb59e89347714bcde51a0b85971f32e1b4eb9535
Alexis <descalexis@gmail.com>
2017-02-25 20:28:28 -0500

    Secrets hidden

--- server/config/secrets.js
+++ server/config/secrets.js
@@ -1,11 +1,8 @@
-/** Important **/
-/** You should not be committing this file to GitHub **/
-/** Repeat: DO! NOT! COMMIT! THIS! FILE! TO! YOUR! REPO! **/
 export const sessionSecret = process.env.SESSION_SECRET || 'Your Session Secret goes here';
 export const google = {
-  clientID: process.env.GOOGLE_CLIENTID || '319371613561-3u1prbufneej1q6hasq9gqp3gsujaeqq.apps.googleusercontent.com',
-  clientSecret: process.env.GOOGLE_SECRET || 'qdl9MJKBRNVBzSWZimOvzo71',
-  callbackURL: process.env.GOOGLE_CALLBACK || '/auth/google/callback'
+  clientID: process.env.GOOGLE_CLIENTID,
+  clientSecret: process.env.GOOGLE_SECRET,
+  callbackURL: process.env.GOOGLE_CALLBACK
 };
 
 export default {
