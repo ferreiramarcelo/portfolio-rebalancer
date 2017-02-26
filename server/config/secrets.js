2832a69afd7badfefbde7a265433a6fc39ba6b7d
Alexis <descalexis@gmail.com>
2017-02-25 20:33:56 -0500

    Updated google OAuth

--- server/config/secrets.js
+++ server/config/secrets.js
@@ -1,3 +1,6 @@
+/** Important **/
+/** You should not be committing this file to GitHub **/
+/** Repeat: DO! NOT! COMMIT! THIS! FILE! TO! YOUR! REPO! **/
 export const sessionSecret = process.env.SESSION_SECRET || 'Your Session Secret goes here';
 export const google = {
   clientID: process.env.GOOGLE_CLIENTID,
