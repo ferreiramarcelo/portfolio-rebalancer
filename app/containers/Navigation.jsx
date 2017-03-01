52fe3b204aaf15050b12232b5d357aacc7219a17
Alexis <descalexis@gmail.com>
2017-02-28 19:55:53 -0500

    Fixed state loss on going to /login

--- app/containers/Navigation.jsx
+++ app/containers/Navigation.jsx
@@ -24,7 +24,7 @@ const Navigation = ({user, logOut}) => {
       <FlatButton
                 labelStyle={{ color: 'white', fontSize: '16px' }}
                 className={cx('navigation-button-right')}
-                onClick={user.authenticated
+                onTouchTap={user.authenticated
                                ? logOut
                                : null}
                 containerElement={user.authenticated
