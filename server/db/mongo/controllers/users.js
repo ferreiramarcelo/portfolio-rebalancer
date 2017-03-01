b75ceb966d16687c5781faeba904e3dc202b0c20
Alexis <descalexis@gmail.com>
2017-02-28 22:03:59 -0500

    Sending dummy email on registration

--- server/db/mongo/controllers/users.js
+++ server/db/mongo/controllers/users.js
@@ -1,61 +1,103 @@
 import passport from 'passport';
 import User from '../models/user';
-
+import Nodemailer from 'nodemailer';
 /**
  * POST /login
  */
-export function login(req, res, next) {
+export function login( req, res, next ) {
   // Do email and password validation for the server
-  passport.authenticate('local', (authErr, user, info) => {
-    if (authErr) return next(authErr);
-    if (!user) {
-      return res.status(401).json({ message: info.message });
+  passport.authenticate( 'local', (authErr, user, info) => {
+    if ( authErr )
+      return next( authErr );
+    if ( !user ) {
+      return res.status( 401 ).json( {
+        message: info.message
+      } );
     }
     // Passport exposes a login() function on req (also aliased as
     // logIn()) that can be used to establish a login session
-    return req.logIn(user, (loginErr) => {
-      if (loginErr) return res.status(401).json({ message: loginErr });
-      return res.status(200).json({
+    return req.logIn( user, (loginErr) => {
+      if ( loginErr )
+        return res.status( 401 ).json( {
+          message: loginErr
+        } );
+      return res.status( 200 ).json( {
         message: 'You have been successfully logged in.'
-      });
-    });
-  })(req, res, next);
+      } );
+    } );
+  } )( req, res, next );
 }
 
 /**
  * POST /logout
  */
-export function logout(req, res) {
+export function logout( req, res ) {
   // Do email and password validation for the server
   req.logout();
-  res.redirect('/');
+  res.redirect( '/' );
 }
 
 /**
  * POST /signup
  * Create a new local account
  */
-export function signUp(req, res, next) {
-  const user = new User({
+export function signUp( req, res, next ) {
+  const user = new User( {
     email: req.body.email,
     password: req.body.password
-  });
+  } );
 
-  User.findOne({ email: req.body.email }, (findErr, existingUser) => {
-    if (existingUser) {
-      return res.status(409).json({ message: 'Account with this email address already exists!' });
+  User.findOne( {
+    email: req.body.email
+  }, (findErr, existingUser) => {
+    if ( existingUser ) {
+      return res.status( 409 ).json( {
+        message: 'Account with this email address already exists!'
+      } );
     }
 
-    return user.save((saveErr) => {
-      if (saveErr) return next(saveErr);
-      return req.logIn(user, (loginErr) => {
-        if (loginErr) return res.status(401).json({ message: loginErr });
-        return res.status(200).json({
+    return user.save( (saveErr) => {
+
+      // Send confirmation email
+      // create reusable transporter object using the default SMTP transport
+      let transporter = Nodemailer.createTransport( {
+        service: '"Postmark"',
+        auth: {
+          user: process.env.POSTMARK_API_TOKEN,
+          pass: process.env.POSTMARK_API_TOKEN
+        }
+      } );
+
+      // setup email data with unicode symbols
+      let mailOptions = {
+        from: '"Portfolio Rebalancer" <noreply@portfoliorebalancer.com>', // sender address
+        to: 'descalexis@gmail.com, alexisdeschampsqc@gmail.com', // list of receivers
+        subject: 'Confirm your Portfolio Rebalancer email', // Subject line
+        text: 'Thanks for registering for Portfolio Rebalancer. ', // plain text body
+        html: '<p>Thanks for registering for PortfolioRebalancer.com! Click the following link to confirm your email: www.dummylink.com.</p>' // html body
+      };
+
+      // send mail with defined transport object
+      transporter.sendMail( mailOptions, (error, info) => {
+        if ( error ) {
+          return console.log( error );
+        }
+        console.log( 'Message %s sent: %s', info.messageId, info.response );
+      } );
+
+      if ( saveErr )
+        return next( saveErr );
+      return req.logIn( user, (loginErr) => {
+        if ( loginErr )
+          return res.status( 401 ).json( {
+            message: loginErr
+          } );
+        return res.status( 200 ).json( {
           message: 'You have been successfully logged in.'
-        });
-      });
-    });
-  });
+        } );
+      } );
+    } );
+  } );
 }
 
 export default {
