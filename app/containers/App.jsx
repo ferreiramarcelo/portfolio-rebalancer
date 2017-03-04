ce211771b38da04f50cd6307536f0c868a3603fa
Alexis <descalexis@gmail.com>
2017-03-04 18:41:54 -0500

    Changed route to be tracked without slash in Navigation.jsx

--- app/containers/App.jsx
+++ app/containers/App.jsx
@@ -4,14 +4,14 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { grey100, grey500, white, fullBlack, } from 'material-ui/styles/colors';
-import {Tabs, Tab} from 'material-ui/Tabs';
+import { Tabs, Tab } from 'material-ui/Tabs';
 import Navigation from '../containers/Navigation';
 import Message from '../containers/Message';
 import styles from '../css/main';
 
 injectTapEventPlugin();
-const cx = classNames.bind(styles);
-const muiTheme = getMuiTheme({
+const cx = classNames.bind( styles );
+const muiTheme = getMuiTheme( {
   fontFamily: 'Roboto, sans-serif',
   zIndex: {
   },
@@ -33,20 +33,18 @@ const muiTheme = getMuiTheme({
   avatar: {
     borderColor: null,
   },
-});
+} );
 
 const App = ({children}) => {
-  return (
-    <MuiThemeProvider muiTheme={muiTheme}>
-      <div className={cx('app')}>
-        <Navigation tab={children.props.location.pathname}/>
-        <Message />
-        <div className={cx('app-children')}>
-          { children }
-        </div>
-      </div>
-    </MuiThemeProvider>
-  );
+  return ( <MuiThemeProvider muiTheme={ muiTheme }>
+             <div className={ cx( 'app' ) }>
+               <Navigation tab={ children.props.location.pathname.slice( 1, children.props.location.pathname.length ) } />
+               <Message />
+               <div className={ cx( 'app-children' ) }>
+                 { children }
+               </div>
+             </div>
+           </MuiThemeProvider> );
 };
 
 App.propTypes = {
--- app/containers/Navigation.jsx
+++ app/containers/Navigation.jsx
@@ -1,62 +1,65 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
+import { Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle } from 'material-ui/Toolbar';
 import FlatButton from 'material-ui/FlatButton';
 import { Tabs, Tab } from 'material-ui/Tabs';
+import Slider from 'material-ui/Slider';
 import classNames from 'classnames/bind';
 import { logOut } from '../actions/users';
 import styles from '../css/containers/navigation';
+
 
 const cx = classNames.bind( styles );
 
 const Navigation = ({tab, user, logOut}) => {
 
   const getLogInTab = function getLogInTab() {
-    return (
-    <Tab
-         value="/login"
-         containerElement={ <Link to="/login" /> }
-         label={ 'LOG IN' } />
-    );
+      return (
+      <Tab
+           value="login"
+           containerElement={ <Link to="/login" /> }
+           label={ 'LOG IN' } />
+      );
   };
   const logInTab = getLogInTab();
 
   const getRegisterTab = function getRegisterTab() {
-    return (
-    <Tab
-         value="/register"
-         containerElement={ <Link to="/register" /> }
-         label={ 'REGISTER' } />
-    );
+      return (
+      <Tab
+           value="register"
+           containerElement={ <Link to="/register" /> }
+           label={ 'REGISTER' } />
+      );
   };
   const registerTab = getRegisterTab();
 
   const getLoggedInAsIdentifier = function getLoggedInAsIdentifier( authenticated, email ) {
-    return (
-    <span className={ cx( 'logged-in-as-identifier' ) }>{ email }</span>
-    );
+      return (
+      <span className={cx('logged-in-as-identifier')} >{ email }</span>
+      );
   };
   const loggedInAsIdentifier = getLoggedInAsIdentifier( user.authenticated, user.email );
 
   const getLogOutButton = function getLogOutButton( authenticated ) {
-    return (
-    <FlatButton
-                onTouchTap={ logOut }
-                label={ "LOG OUT" }
-                className={ cx( 'log-out-button' ) } />
-    );
+      return (
+      <FlatButton
+                  onTouchTap={ logOut }
+                  label={ "LOG OUT" }
+                  className={cx('log-out-button')} />
+      );
   };
   const logOutButton = getLogOutButton( user.authenticated );
 
   const getUnauthenticatedTabs = function getUnauthenticatedTabs() {
     return (<Tabs value={ tab }>
               <Tab
-                   value={"/"}
+                   value=""
                    label="PR"
                    containerElement={ <Link to="/" /> }>
               </Tab>
               <Tab
-                   value="/about"
+                   value="about"
                    label="ABOUT"
                    containerElement={ <Link to="/about" /> }>
               </Tab>
@@ -68,29 +71,31 @@ const Navigation = ({tab, user, logOut}) => {
 
   const getAuthenticatedNav = function getAuthenticatedNav() {
     return (
-    <div className={ cx( 'authenticated-container' ) }>
-      <div className={ cx( 'authenticated-container-tabs' ) }>
-        <Tabs value={ tab }>
-          <Tab
-               value={"/"}
-               label="PR"
-               containerElement={ <Link to="/" /> }>
-          </Tab>
-          <Tab
-               value="/about"
-               label="ABOUT"
-               containerElement={ <Link to="/about" /> }>
-          </Tab>
-          <Tab
-               value="/account"
-               label="ACCOUNT"
-               containerElement={ <Link to="/account" /> }>
-          </Tab>
-        </Tabs>
-      </div>
-      <div className={ cx( 'authenticated-container-log-out' ) }>
-        { logOutButton }
-      </div>
+    <div className={cx('authenticated-container')}>
+      <div className={cx('authenticated-container-tabs')} >
+      <Tabs
+            value={ tab }
+          >
+        <Tab
+             value=""
+             label="PR"
+             containerElement={ <Link to="/" /> }>
+        </Tab>
+        <Tab
+             value="about"
+             label="ABOUT"
+             containerElement={ <Link to="/about" /> }>
+        </Tab>
+        <Tab
+             value="account"
+             label="ACCOUNT"
+             containerElement={ <Link to="/account" /> }>
+        </Tab>
+      </Tabs>
+    </div>
+    <div className={cx('authenticated-container-log-out')}>
+      { logOutButton }
+    </div>
     </div>);
   }
   const authenticatedNav = getAuthenticatedNav();
