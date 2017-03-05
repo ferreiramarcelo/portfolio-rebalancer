86eeebc80e6bef61b6dd3e928d4e5e15daa4a574
Alexis <descalexis@gmail.com>
2017-03-04 22:02:55 -0500

    Heroku deploy

--- app/containers/Navigation.jsx
+++ app/containers/Navigation.jsx
@@ -23,17 +23,7 @@ const Navigation = ({tab, user, logOut}) => {
       );
   };
   const logInTab = getLogInTab();
-
-  const getRegisterTab = function getRegisterTab() {
-      return (
-      <Tab
-           value="register"
-           containerElement={ <Link to="/register" /> }
-           label={ 'REGISTER' } />
-      );
-  };
-  const registerTab = getRegisterTab();
-
+  
   const getLoggedInAsIdentifier = function getLoggedInAsIdentifier( authenticated, email ) {
       return (
       <span className={cx('logged-in-as-identifier')} >{ email }</span>
@@ -52,57 +42,14 @@ const Navigation = ({tab, user, logOut}) => {
   const logOutButton = getLogOutButton( user.authenticated );
 
   const getUnauthenticatedTabs = function getUnauthenticatedTabs() {
-    return (<Tabs value={ tab }>
-              <Tab
-                   value=""
-                   label="PR"
-                   containerElement={ <Link to="" /> }>
-              </Tab>
-              <Tab
-                   value="about"
-                   label="ABOUT"
-                   containerElement={ <Link to="/about" /> }>
-              </Tab>
-              { logInTab }
-              { registerTab }
-            </Tabs>);
+    return null;
   }
   const unauthenticatedTabs = getUnauthenticatedTabs();
 
-  const getAuthenticatedNav = function getAuthenticatedNav() {
-    return (
-    <div className={cx('authenticated-container')}>
-      <div className={cx('authenticated-container-tabs')} >
-      <Tabs
-            value={ tab }
-          >
-        <Tab
-             value=""
-             label="PR"
-             containerElement={ <Link to="" /> }>
-        </Tab>
-        <Tab
-             value="about"
-             label="ABOUT"
-             containerElement={ <Link to="/about" /> }>
-        </Tab>
-        <Tab
-             value="account"
-             label="ACCOUNT"
-             containerElement={ <Link to="/account" /> }>
-        </Tab>
-      </Tabs>
-    </div>
-    <div className={cx('authenticated-container-log-out')}>
-      { logOutButton }
-    </div>
-    </div>);
-  }
-  const authenticatedNav = getAuthenticatedNav();
 
   const getNavigation = function getNavigation( givenAuthenticated ) {
     if ( givenAuthenticated ) {
-      return authenticatedNav;
+      return unauthenticatedTabs;
     }
     return unauthenticatedTabs;
   }
