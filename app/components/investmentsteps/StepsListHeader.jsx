c2eb2d7720f5306e1ee3963e668d7ed9abd56067
Alexis <descalexis@gmail.com>
2017-03-21 14:56:52 -0400

    Heroku deploy fix attempt

--- app/components/investmentsteps/StepsListHeader.jsx
+++ app/components/investmentsteps/StepsListHeader.jsx
@@ -9,22 +9,7 @@ const cx = classNames.bind(styles);
 const StepsListHeader = ({showWholeUnits, showPartialUnits, showCashAmounts, changeShowWholeUnits, changeShowPartialUnits, changeShowCashAmounts, rebalancingSteps}) => {
 
   const getStepsListHeaderElements = function getStepsListHeaderElements() {
-    if (!rebalancingSteps.balanceByInvesting) {
-      return <div></div>;
-    }
-    return (<div>
-              <div className={ cx('steps-list-header') }>
-                <div className={ cx('steps-list-header-text-container') }>
-                  <span className={ cx('steps-list-header-text') }>Steps List</span>
-                </div>
-                <div className={ cx('steps-list-header-checkboxes') }>
-                  <Checkbox checked={ showWholeUnits } onCheck={ changeShowWholeUnits } label="Whole units" className={ cx('checkbox') } />
-                  <Checkbox checked={ showPartialUnits } onCheck={ changeShowPartialUnits } label="Partial units" className={ cx('checkbox') } />
-                  <Checkbox checked={ showCashAmounts } onCheck={ changeShowCashAmounts } label="Cash amounts" className={ cx('checkbox') } />
-                </div>
-              </div>
-              <StepsList rebalancingSteps={ rebalancingSteps } showWholeUnits={ showWholeUnits } showPartialUnits={ showPartialUnits } showCashAmounts={ showCashAmounts } />
-            </div>);
+    return "ha";
   };
   const stepsListHeaderElements = getStepsListHeaderElements();
 
